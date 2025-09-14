8cdd2cb9cdb0200afa3fb7c83e81d3d59c0b7e9b
Carlos Horst <chorstconsult@gmail.com>
2025-09-14 13:25:53 +0000

    continue

--- src/app/(app)/termination/page.tsx
+++
@@ -1,7 +0,0 @@
-'use client';
-
-import TerminationSimulator from '@/components/simulators/termination-simulator';
-
-export default function TerminationPage() {
-    return <TerminationSimulator />
-}
