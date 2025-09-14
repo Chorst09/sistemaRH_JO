053e6999066d384eb9e7d6382cc6da9031ba18e7
Carlos Horst <chorstconsult@gmail.com>
2025-09-14 13:43:57 +0000

    agora no simulador criar um simulador especifico para empregados domesti

--- src/app/(app)/simulators/page.tsx
+++ src/app/(app)/simulators/page.tsx
@@ -12,6 +12,7 @@ import VacationSimulator from '@/components/simulators/vacation-simulator';
 import TerminationSimulator from '@/components/simulators/termination-simulator';
 import CltVsPjSimulator from '@/components/simulators/clt-vs-pj-simulator';
 import { Calculator } from 'lucide-react';
+import DomesticSimulator from '@/components/simulators/domestic-simulator';
 
 export default function SimulatorsPage() {
   return (
@@ -24,10 +25,11 @@ export default function SimulatorsPage() {
           <p className="text-muted-foreground">Projete cenários e tome decisões informadas.</p>
       </div>
       <Tabs defaultValue="termination" className="w-full">
-        <TabsList className="grid w-full grid-cols-3">
-          <TabsTrigger value="termination">Simulador de Rescisão</TabsTrigger>
-          <TabsTrigger value="vacation">Simulador de Férias</TabsTrigger>
+        <TabsList className="grid w-full grid-cols-4">
+          <TabsTrigger value="termination">Rescisão</TabsTrigger>
+          <TabsTrigger value="vacation">Férias</TabsTrigger>
           <TabsTrigger value="clt-vs-pj">CLT vs. PJ</TabsTrigger>
+          <TabsTrigger value="domestic">Empregado Doméstico</TabsTrigger>
         </TabsList>
         <TabsContent value="termination">
             <TerminationSimulator />
@@ -37,6 +39,9 @@ export default function SimulatorsPage() {
         </TabsContent>
         <TabsContent value="clt-vs-pj">
             <CltVsPjSimulator />
+        </TabsContent>
+        <TabsContent value="domestic">
+            <DomesticSimulator />
         </TabsContent>
       </Tabs>
     </div>
